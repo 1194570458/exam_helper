9765c27f65ce4b18cb2944da8bd4741743347e1b
huang <794821788@qq.com>
2018-07-16 13:28:03 +0800

    test

--- src/main/java/com/kason/app/controller/UserController.java
+++ src/main/java/com/kason/app/controller/UserController.java
@@ -7,9 +7,6 @@ import com.kason.app.utils.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
-import java.util.Date;
-
 @RestController
 @CrossOrigin
 public class UserController {
@@ -104,5 +101,8 @@ public class UserController {
         User user = userService.getUser(id);
         return ResultUtil.success(user);
     }
+
+
+
 
 }
