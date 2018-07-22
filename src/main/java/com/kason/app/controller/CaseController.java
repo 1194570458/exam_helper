b7fde6d9335ab5f714520e9599fca00b54b37b4f
huang <794821788@qq.com>
2018-07-22 21:54:31 +0800

    html

--- src/main/java/com/kason/app/controller/CaseController.java
+++ src/main/java/com/kason/app/controller/CaseController.java
@@ -2,13 +2,13 @@ package com.kason.app.controller;
 
 import com.kason.app.entity.Case;
 import com.kason.app.entity.Result;
-import com.kason.app.entity.Synthesize;
 import com.kason.app.service.CaseService;
 import com.kason.app.utils.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
@@ -17,11 +17,6 @@ public class CaseController {
     @Autowired
     private CaseService caseService;
 
-    /**
-     * 添加或者更新案例
-     * @param c
-     * @return
-     */
     @PostMapping("/case")
     public Result<Case> addOrUpdateCase(@RequestBody Case c) {
         Case ca;
@@ -36,48 +31,5 @@ public class CaseController {
         }
         return ResultUtil.success(ca);
     }
-
-    /**
-     * 获取全部案例
-     * @return
-     */
-    @GetMapping("/cases")
-    public Result<List<Case>> getCases(){
-        return ResultUtil.success(caseService.getCases());
-    }
-
-    /**
-     * 根据id删除案例
-     * @param id
-     * @return
-     */
-    @PostMapping("/case/{id}")
-    public Result delCase(@PathVariable("id") Integer id){
-        caseService.delCase(id);
-        return ResultUtil.success();
-    }
-
-    /**
-     * 根据id获取案例
-     * @param id
-     * @return
-     */
-    @GetMapping("/case/{id}")
-    public Result<Case> getCase(@PathVariable("id")Integer id){
-        Case c = caseService.getCase(id);
-        return ResultUtil.success(c);
-    }
-
-    /**
-     * 根据证书id和章节id获取案例
-     * @param cerId
-     * @param proId
-     * @return
-     */
-    @GetMapping("/case/{cerId}/{proId}")
-    public Result<Case> getCase(@PathVariable("cerId") Integer cerId, @PathVariable("proId")Integer proId){
-        return ResultUtil.success(caseService.getCase(cerId,proId));
-    }
-
 
 }
